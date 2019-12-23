de7ed0aa772fbad543221ea17a4efc43849ccd3e
setohima <raito1261@gmail.com>
2019-12-23 14:43:03 +0000

    modify setting

--- guidance_emu/urls.py
+++ guidance_emu/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
 from . import views
 
+from django.views.generic import TemplateView
+from django.views.decorators.cache import cache_control
+
 urlpatterns = [
     path('', views.top, name='top'),
     path('help/', views.help, name='help'),
     path('privacypolicy/', views.privacypolicy, name='privacypolicy'),
+    path('sw.js/', (TemplateView.as_view(template_name="sw.js",content_type='application/javascript', )), name='sw.js'),
 ]

cd744d784333df6670e7765ef174ed19be51bf0a
setohima <raito1261@gmail.com>
2019-12-23 14:36:56 +0000

    add PWA

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
 from django.urls import include, path
 
+from django.views.generic import TemplateView
+from django.views.decorators.cache import cache_control
+
 urlpatterns = [
     path('', include('guidance_emu.urls')),
     #path('admin/', admin.site.urls),
+    path('sw.js', (TemplateView.as_view(template_name="sw.js",content_type='application/javascript', )), name='sw.js'),
 ]
